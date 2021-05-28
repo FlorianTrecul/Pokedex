83abc2432bbcdb3b36a4c9d612fd2c84fe53398f
Florian TRECUL <trecul.florian@gmail.com>
2021-05-28 16:57:50 +0200

    Add progress loader part for pokemon list screen

--- app/src/main/java/com/floriantrecul/pokedex/ui/screens/list/PokemonListScreen.kt
+++ app/src/main/java/com/floriantrecul/pokedex/ui/screens/list/PokemonListScreen.kt
@@ -1,12 +1,14 @@
 package com.floriantrecul.pokedex.ui.screens.list
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
@@ -15,6 +17,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -22,6 +25,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.floriantrecul.pokedex.R
+import com.floriantrecul.pokedex.ui.components.CircularProgressLoader
 import com.floriantrecul.pokedex.ui.components.PokemonCard
 import com.floriantrecul.pokedex.ui.data.model.PokemonItemUiModel
 
@@ -36,6 +40,7 @@ fun PokemonListScreen(
     val isLoading by remember { viewModel.isLoading }
 
     Surface(
+        modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background,
     ) {
         Column(
@@ -59,6 +64,7 @@ fun PokemonListScreen(
             PokemonList(
                 pokemons = pokemonList,
                 endReached = endReached,
+                isLoading = isLoading,
                 loadPokemons = viewModel::loadPokemons,
                 navigatePokemonDetailsScreen = navigatePokemonDetailsScreen
             )
@@ -70,21 +76,32 @@ fun PokemonListScreen(
 fun PokemonList(
     pokemons: List<PokemonItemUiModel>,
     endReached: Boolean,
+    isLoading: Boolean,
     loadPokemons: () -> Unit,
     navigatePokemonDetailsScreen: (Int) -> Unit
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         itemsIndexed(pokemons) { index, pokemon ->
-            if (index == pokemons.lastIndex && !endReached) {
+            if (index == pokemons.lastIndex && !endReached && !isLoading) {
                 loadPokemons()
             }
             PokemonCard(
                 pokemon = pokemon,
                 navigatePokemonDetailsScreen = navigatePokemonDetailsScreen
+            )
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (isLoading) {
+            CircularProgressLoader(
+                modifier = Modifier.size(128.dp),
+                color = MaterialTheme.colors.onSurface
             )
         }
     }
